3f105cf32e89a589f01ba175d0e3708033b5c044
Vincent Joy <vincentjoyhere@gmail.com>
2025-06-13 15:11:07 +0530

    Managed the loadid states and handled the request errors

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,4 +1,4 @@
-import { useContext, useLayoutEffect } from 'react';
+import { useContext, useLayoutEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import ExpenseForm from '../components/ManageExpense/ExpenseForm';
@@ -6,8 +6,11 @@ import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
 import { ExpensesContext } from '../store/expenses-context';
 import { storeExpense, updateExpense, deleteExpense } from '../util/http';
+import LoadingOverlay from '../components/UI/LoadingOverlay';
 
 function ManageExpense({ route, navigation }) {
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const [error, setError] = useState();
     const expensesCtx = useContext(ExpensesContext);
 
     const editedExpenseId = route.params?.expenseId;
@@ -22,9 +25,15 @@ function ManageExpense({ route, navigation }) {
     }, [navigation, isEditing]);
 
     async function deleteExpenseHandler() {
-        await deleteExpense(editedExpenseId)
-        expensesCtx.deleteExpense(editedExpenseId);
-        navigation.goBack();
+        setIsSubmitting(true);
+        try {
+            await deleteExpense(editedExpenseId);
+            expensesCtx.deleteExpense(editedExpenseId);
+            navigation.goBack();
+        } catch (error) {
+            setError('Could not delete expense - please try again later!');
+            setIsSubmitting(false);
+        }
     }
 
     function cancelHandler() {
@@ -32,14 +41,28 @@ function ManageExpense({ route, navigation }) {
     }
 
     async function confirmHandler(expenseData) {
-        if (isEditing) {
-            expensesCtx.updateExpense(editedExpenseId, expenseData);
-            await updateExpense(editedExpenseId, expenseData)
-        } else {
-            const id = await storeExpense(expenseData)
-            expensesCtx.addExpense({...expenseData, id: id});
+        setIsSubmitting(true);
+        try {
+            if (isEditing) {
+                expensesCtx.updateExpense(editedExpenseId, expenseData);
+                await updateExpense(editedExpenseId, expenseData);
+            } else {
+                const id = await storeExpense(expenseData);
+                expensesCtx.addExpense({ ...expenseData, id: id });
+            }
+            navigation.goBack();
+        } catch (error) {
+            setError('Could not save data - please try again later!');
+            setIsSubmitting(false);
         }
-        navigation.goBack();
+    }
+
+    if (error && !isSubmitting) {
+        return <ErrorOverlay message={error} />;
+    }
+
+    if (isSubmitting) {
+        return <LoadingOverlay />
     }
 
     return (
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,20 +1,36 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
 import { fetchExpenses } from '../util/http';
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 
 function RecentExpenses() {
+    const [isFetching, setIsFetching] = useState(true)
+    const [error, setError] = useState();
     const expensesCntx = useContext(ExpensesContext)
 
     useEffect(() => {
         async function getExpenses() {
-            const expenses = await fetchExpenses();
-            expensesCntx.setExpenses(expenses);
+            try {
+                const expenses = await fetchExpenses();
+                expensesCntx.setExpenses(expenses);
+            } catch (error) {
+                setError('Could not fetch expenses!');
+            }
+            setIsFetching(false);
         }
 
         getExpenses();
     }, []);
+
+    if (error && !isFetching) {
+        return <ErrorOverlay message={error} />;
+    }
+
+    if (isFetching) {
+        return <LoadingOverlay />
+    }
 
     const recentExpenses = expensesCntx.expenses.filter((expense) => {
         const today = new Date()
